74dcad3fb73883f4c10b1f1dba8e60fb40457087
Deyvi-dev <deyvisson.margalhaes@gmail.com>
2021-11-30 02:44:04 -0300

    day-2

---
+++ day-2/script.js
@@ -0,0 +1,33 @@
+// Declare second integer, double, and String variables.
+// Read and save an integer, double, and String to your variables.
+// Print the sum of both integer variables on a new line.
+// Print the sum of the double variables on a new line.
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    
+    var myi = 12
+    var myd = 4.0
+    var mys = "is the best place to learn and practice coding!"
+    
+ 
+   console.log(myi + i);
+   console.log((myd + d).toFixed(1));
+   console.log(s + mys);
+
+
+    var u = 3
+    var e = 2.8
+   
+    
+    var myu = 4
+    var mye = 4.0
+    var myf = "is my favorite platform!"
+    
+ 
+   console.log(myu + u);
+   console.log((mye + e).toFixed(1));
+   console.log(s + myf);
